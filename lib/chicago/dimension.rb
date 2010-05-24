c96353a5bff1331e5e7dfc84a9296297cada1a0c
Roland Swingler <roland.swingler@gmail.com>
2010-05-24 11:38:45 +0100

    Using Dimension.define instead of Dimension.new.

--- lib/chicago/dimension.rb
+++ lib/chicago/dimension.rb
@@ -11,16 +11,24 @@ module Chicago
     attr_reader :column_definitions
 
     # Creates a new dimension, named +name+
-    def initialize(name)
-      @table_name = "#{name}_dimension".to_sym
-      @name = name.to_sym
-      @column_definitions = []
+    def self.define(name, &block)
+      dimension = self.new(name)
+      dimension.instance_eval(&block) if block_given?
+      dimension
     end
 
     # Define a set of columns for this dimension. See ColumnGroupBuilder
     # for details.
     def columns(&block)
       @column_definitions += Schema::ColumnGroupBuilder.new(&block).column_definitions
+    end
+
+    protected
+
+    def initialize(name)
+      @table_name = "#{name}_dimension".to_sym
+      @name = name.to_sym
+      @column_definitions = []
     end
   end
 end
--- spec/dimension_spec.rb
+++ spec/dimension_spec.rb
@@ -6,15 +6,15 @@ describe Chicago::Dimension do
   end
 
   it "should be constructed with a dimension name" do
-    Dimension.new(:user).name.should == :user
+    Dimension.define(:user).name.should == :user
   end
 
   it "should return a symbol from #name" do
-    Dimension.new('user').name.should == :user
+    Dimension.define('user').name.should == :user
   end
 
   it "should have a table name" do
-    Dimension.new(:user).table_name.should == :user_dimension
+    Dimension.define(:user).table_name.should == :user_dimension
   end
 
   it "should define a group of columns" do
@@ -23,8 +23,10 @@ describe Chicago::Dimension do
     Schema::ColumnGroupBuilder.should_receive(:new).and_return(mock_builder)
     mock_builder.should_receive(:column_definitions).and_return([column])
 
-    dd = Dimension.new(:user)
-    dd.columns { varchar :username }
+    dd = Dimension.define(:user) do
+      columns { varchar :username }
+    end
+
     dd.column_definitions.should == [column]
   end
 end
