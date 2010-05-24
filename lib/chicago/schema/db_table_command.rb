a94297794b249fcfbd6692aafba83ec07a91db90
Roland Swingler <roland.swingler@gmail.com>
2010-05-24 10:39:54 +0100

    Refactoring DbTableCommand and using new attributes.

---
+++ lib/chicago/schema/db_table_command.rb
@@ -0,0 +1,111 @@
+module Chicago
+  module Schema
+    # A command to alter or create a table to bring it in line with a
+    # set of column definitions.
+    #
+    # This is an abstract class and should not be instantiated.
+    class DbTableCommand
+      # Returns a DbTableCommand subclass appropriate for modifying or creating tables.
+      def self.for_table(db, table_name, columns)
+        subclass = db.table_exists?(table_name) ? AlterDbTableCommand : CreateDbTableCommand
+        subclass.new(db, table_name, columns)
+      end
+
+      def initialize(db, table_name, columns)
+        @db = db
+        @table_name = table_name
+        @columns = columns
+        @type_converter = TypeConverters::DbTypeConverter.for_db(@db)
+        build_generator
+      end
+
+      # Execute the command. Overriden in subclasses.
+      def execute ; end
+      
+      # Returns the associated Sequel Generator used to create DDL statements. Overriden in subclasses.
+      def generator ; end
+
+      protected
+
+      # Build the Sequel::Schema::*Generator - Overriden in subclasses.
+      def build_generator ; end
+    end
+
+
+    # A command to create a new table.
+    #
+    # This shouldn't be created directly - use DbTableCommand.for_table
+    class CreateDbTableCommand < DbTableCommand #:nodoc:
+      def generator
+        @generator ||= Sequel::Schema::Generator.new(@db)
+      end
+
+      def execute
+        @db.create_table(@table_name, :generator => generator)      
+      end
+
+      protected
+
+      def build_generator
+        @columns.each do |column|
+          generator.column(column.name, @type_converter.db_type(column), column.sequel_column_options)
+        end
+      end
+    end
+
+
+    # A command to alter a prexisting table.
+    #
+    # This shouldn't be created directly - use DbTableCommand.for_table
+    class AlterDbTableCommand < DbTableCommand #:nodoc:
+      def generator
+        @generator ||= Sequel::Schema::AlterTableGenerator.new(@db)
+      end
+
+      def execute        
+        @db.alter_table(@table_name, generator) unless generator.operations.empty?
+      end
+
+      protected
+
+      def build_generator
+        current_columns, new_columns = @columns.partition {|c| @db[@table_name].columns.include?(c.name) }
+        create_new_columns(generator, new_columns)
+        modify_existing_columns(generator, current_columns)
+      end
+
+      private
+
+      def create_new_columns(generator, new_columns)
+        new_columns.each do |column|
+          generator.add_column(column.name, @type_converter.db_type(column), column.sequel_column_options)
+        end
+      end
+
+      def modify_existing_columns(generator, current_columns)
+        current_columns.each do |column|
+          attrs = @db.schema(@table_name).assoc(column.name).last
+
+          change_type    generator, attrs, column
+          change_null    generator, attrs, column         
+          change_default generator, attrs, column          
+        end
+      end
+
+      def change_type(generator, attrs, column)
+        new_type     = @type_converter.db_type(column)
+        current_type = @type_converter.parse_type_string(attrs[:db_type])
+
+        generator.set_column_type(column.name, new_type, column.sequel_column_options) if new_type != current_type
+      end
+
+      def change_null(generator, attrs, column)
+        generator.set_column_allow_null(column.name, column.null?) if attrs[:allow_null] != column.null?
+      end
+
+      def change_default(generator, attrs, column)
+        generator.set_column_default(column.name, column.default) if attrs[:ruby_default] != column.default
+      end
+    end
+  end
+end
--- spec/alter_db_table_command_spec.rb
+++ spec/alter_db_table_command_spec.rb
@@ -23,11 +23,37 @@ describe Chicago::Schema::AlterDbTableCommand do
     TEST_DB.schema(:test_table).last.first.should == :username
   end
 
+  it "should change an existing column rather than adding a new column" do
+    columns = [ColumnDefinition.new(:name => :id, :type => :float)]
+    Schema::AlterDbTableCommand.new(TEST_DB, :test_table, columns).execute
+    TEST_DB.schema(:test_table).size.should == 1
+  end
+
   it "should change a column's type if the definition has changed" do
     columns = [ColumnDefinition.new(:name => :id, :type => :integer, :min => -1000, :max => 300)]
     Schema::AlterDbTableCommand.new(TEST_DB, :test_table, columns).execute
-    
-    TEST_DB.schema(:test_table).size.should == 1
+
     TEST_DB.schema(:test_table, :reload => true).first.last[:db_type].should == "smallint(6)"
+  end
+
+  it "should change a column's type to unsigned" do
+    columns = [ColumnDefinition.new(:name => :id, :type => :integer, :min => 0, :max => 300)]
+    Schema::AlterDbTableCommand.new(TEST_DB, :test_table, columns).execute
+    
+    TEST_DB.schema(:test_table, :reload => true).first.last[:db_type].should == "smallint(5) unsigned"
+  end
+
+  it "should change a column's null constraint" do
+    columns = [ColumnDefinition.new(:name => :id, :type => :integer, :null => false)]
+    Schema::AlterDbTableCommand.new(TEST_DB, :test_table, columns).execute
+    
+    TEST_DB.schema(:test_table, :reload => true).first.last[:allow_null].should be_false
+  end
+
+  it "should change a column's default" do
+    columns = [ColumnDefinition.new(:name => :id, :type => :integer, :default => 23)]
+    Schema::AlterDbTableCommand.new(TEST_DB, :test_table, columns).execute
+    
+    TEST_DB.schema(:test_table, :reload => true).first.last[:default].to_i.should == 23
   end
 end
