a40401f5c29bc45eed0c673f949ac2bb42e641b7
Roland Swingler <roland.swingler@gmail.com>
2010-06-19 13:17:53 +0100

    Migration file writer.

--- lib/chicago.rb
+++ lib/chicago.rb
@@ -1,10 +1,10 @@
 # Requires go here
 require 'sequel'
+require 'sequel/migration_builder'
 require 'chicago/column_definition'
 require 'chicago/star_schema_table'
 require 'chicago/dimension'
-require 'chicago/table_builder'
+require 'chicago/migration_file_writer'
 require 'chicago/fact'
 require 'chicago/schema/type_converters'
 require 'chicago/schema/column_group_builder'
-require 'chicago/schema'
