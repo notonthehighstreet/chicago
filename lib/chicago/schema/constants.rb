86b3857d36f3e3e49b72cd98d9338215cd68936d
Roland Swingler <roland.swingler@gmail.com>
2010-06-28 12:52:35 +0100

    Adding constants file.

---
+++ lib/chicago/schema/constants.rb
@@ -0,0 +1,9 @@
+module Chicago
+  module Schema
+    TINY_INT_MAX   = 255
+    SMALL_INT_MAX  = 65_535
+    MEDIUM_INT_MAX = 16_777_215
+    INT_MAX        = 4_294_967_295
+    BIG_INT_MAX    = 18_446_744_073_709_551_615
+  end
+end
