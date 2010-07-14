31bc97d2017e138e301b1894adbce458d4df9ec5
Roland Swingler <roland.swingler@gmail.com>
2010-07-14 18:41:14 +0100

    Adding month definition.

---
+++ lib/chicago/month.rb
@@ -0,0 +1,71 @@
+module Chicago
+  module Data
+    class Month
+      include Comparable
+
+      # Returns the full English name of this month
+      attr_reader :name
+
+      # Returns the first three letters of the English name of this month.
+      attr_reader :short_name
+
+      def initialize(name, number)
+        @name = name
+        @short_name = name[0..2]
+        @number = number
+      end
+
+      def <=>(other)
+        to_i <=> other.to_i 
+      end
+
+      # Returns the first day of this month in the given year as a Date.
+      def in(year)
+        Date.new(year, to_i, 1)
+      end
+
+      # Returns the number of this month from 1 to 12
+      def to_i
+        @number
+      end
+
+      # Returns the full name of this month
+      def to_s
+        name
+      end
+
+      ALL = [Month.new("January", 1),
+             Month.new("February", 2),
+             Month.new("March", 3),
+             Month.new("April", 4),
+             Month.new("May", 5),
+             Month.new("June", 6),
+             Month.new("July", 7),
+             Month.new("August", 8),
+             Month.new("September", 9),
+             Month.new("October", 10),
+             Month.new("November", 11),
+             Month.new("December", 12)]
+
+      class << self
+        [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december].each_with_index do |month, i|
+          eval "def #{month} ; ALL[#{i}] ; end"
+        end
+
+        # Returns a month given a name of the month or an integer
+        # between 1 and 12.
+        def parse(identifier)
+          if identifier.kind_of?(Fixnum) && identifier >= 1 && identifier <= 12
+            ALL[identifier - 1]
+          else
+            name = identifier.strip.downcase
+            ALL.find {|month| month.name.downcase == name || month.short_name.downcase == name }
+          end
+        end
+
+        private :new
+      end
+      
+    end
+  end
+end
---
+++ spec/month_spec.rb
@@ -0,0 +1,67 @@
+require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
+
+describe Chicago::Data::Month do
+
+  [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december].each do |month|
+    it "should return a Month for #{month}" do
+      Chicago::Data::Month.send(month).should be_kind_of(Chicago::Data::Month)
+    end
+  end
+
+  it "should have a name" do
+    Chicago::Data::Month.january.name.should == "January"
+  end
+
+  it "should return the name from #to_s" do
+    Chicago::Data::Month.january.to_s.should == "January"
+  end
+
+  it "should have a short name" do
+    Chicago::Data::Month.january.short_name.should == "Jan"
+  end
+
+  it "should have a number" do
+    Chicago::Data::Month.january.to_i.should == 1
+  end
+
+  it "should return a date in a year" do
+    Chicago::Data::Month.january.in(2009).should == Date.new(2009,1,1)
+  end
+
+  it "should be the same instance on mutliple calls to a month name" do
+    Chicago::Data::Month.january.object_id.should == Chicago::Data::Month.january.object_id
+  end
+
+  it "should be comparable" do
+    Chicago::Data::Month.january.should < Chicago::Data::Month.february
+    Chicago::Data::Month.december.should > Chicago::Data::Month.january
+  end
+
+  it "should not be constructable" do
+    lambda { Chicago::Data::Month("Foo", 13) }.should raise_error
+  end
+
+  it "should be parsable from a short string" do
+    Chicago::Data::Month.parse("Jan").should == Chicago::Data::Month.january
+  end
+
+  it "should be parsable from a short string uncaptialized" do
+    Chicago::Data::Month.parse("jan").should == Chicago::Data::Month.january
+  end
+
+  it "should be parsable from a full name" do
+    Chicago::Data::Month.parse("December").should == Chicago::Data::Month.december
+  end
+
+  it "should be parsable from a full name uncaptialized" do
+    Chicago::Data::Month.parse("january").should == Chicago::Data::Month.january
+  end
+
+  it "should be parsable from an integer" do
+    Chicago::Data::Month.parse(1).should == Chicago::Data::Month.january
+  end
+
+  it "should return nil from a string that isn't a month name" do
+    Chicago::Data::Month.parse("maybe").should == nil
+  end
+end
